b7f1582fce79676bb612dd5fc73eecc8bf9d70ac
matthewgani <matthewgani2@gmail.com>
2022-08-03 17:04:20 +0800

    Refactor SpotifyNowPlaying

--- src/spotify/SpotifyNowPlaying.js
+++ src/spotify/SpotifyNowPlaying.js
@@ -2,22 +2,9 @@ import { useEffect, useState } from "react";
 import SpotifyAPI from "./SpotifyAPI";
 import SpotifyLogo from "./SpotifyLogo";
 
-const SpotifyNowPlaying = (props) => {
+const SpotifyNowPlaying = () => {
     const [loading, setLoading] = useState(true);
     const [result, setResult] = useState({});
-    // useEffect(() => {
-
-    //     Promise.all([
-    //         getNowPlayingItem(
-    //             props.client_id,
-    //             props.client_secret,
-    //             props.refresh_token
-    //         ),
-    //     ]).then((results) => {
-    //         setResult(results[0]);
-    //         setLoading(false);
-    //     });
-    // });
 
     useEffect(() => {
       SpotifyAPI.getNowPlayingItem()
@@ -26,65 +13,31 @@ const SpotifyNowPlaying = (props) => {
           setLoading(false)
         })
     })
-    
-    if (loading) {
-      return (
-        <div>
-          <p>Connecting to spotify...</p>
-        </div>
-      )
-    }
-    else if (!loading && !result.isPlaying){
-      return (
-        <div>
-          <SpotifyLogo />
-          <span>Currently not playing a song!</span>
-        </div>
-      )
-    }
-    else if(!loading && result.isPlaying) {
-      return (
-      <div>
-        <div>
-            <SpotifyLogo />
-            <span>Now playing</span>
-        </div>
-        <div>
-            <img src={result.albumImageUrl} alt={`${result.title} album art`}/>
-            {/* <PlayingAnimation /> */}
-            <a href={result.songUrl} target="_blank" rel="noreferrer">{result.title}</a>
-            <p>{result.artist}</p>
-        </div>
-      </div>
-      )
-    }
 
-
-    // return (
-    //     <div>
-    //         {loading && <p>Loading...</p>}
-    //         {!loading && (result['isPlaying'] === false)(
-    //             <div>
-    //                 <SpotifyLogo />
-    //                 <span>Currently offline"</span>
-    //             </div>
-    //         )}
-    //         {!loading && (result['isPlaying'] === true) (
-    //             <div>
-    //                 <div>
-    //                     <SpotifyLogo />
-    //                     <span>Now playing</span>
-    //                 </div>
-    //                 <div>
-    //                     <img src={result.albumImageUrl} alt={`${result.title} album art`}/>
-    //                     {/* <PlayingAnimation /> */}
-    //                     <a href={result.songUrl} target="_blank" rel="noreferrer">{result.title}</a>
-    //                     <p>{result.artist}</p>
-    //                 </div>
-    //             </div>
-    //         )}
-    //     </div>
-    // )
+    return (
+        <div>
+            {loading && <p>Connecting to spotify...</p>}
+            {(!loading && !result.isPlaying) && (
+                <div>
+                    <SpotifyLogo />
+                    <span>Currently not playing a song!</span>
+                </div>
+            )}
+            {(!loading && result.isPlaying) && (
+                <div>
+                    <div>
+                        <SpotifyLogo />
+                        <span>Now playing</span>
+                    </div>
+                    <div>
+                        <img src={result.albumImageUrl} alt={`${result.title} album art`}/>
+                        <a href={result.songUrl} target="_blank" rel="noreferrer">{result.title}</a>
+                        <p>{result.artist}</p>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
 };
 
 export default SpotifyNowPlaying
